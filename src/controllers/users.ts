7a6c90fdb1a2e24ebe1453e72c2dda6bac76680d
TahirReyaz <tahirreyaz5@gmail.com>
2023-12-29 18:28:13 +0530

    Elaborated user schema related to profile and method for updating it

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,51 +1,61 @@
-import express from 'express';
+import express from "express";
 
-import { deleteUserById, getUserById, getUsers } from '../db/users.js';
+import { deleteUserById, getUserById, getUsers } from "../db/users.js";
 
-export const getAllUsers = async (req: express.Request, res: express.Response) => {
-    try {
-        const users = await getUsers();
+export const getAllUsers = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const users = await getUsers();
 
-        return res.status(200).json(users);
-        
-    } catch (error) {
-        console.log(error);
-        return res.sendStatus(400);
+    return res.status(200).json(users);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
+export const deleteUser = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const { id } = req.params;
+
+    const deletedUser = await deleteUserById(id);
+
+    return res.json(deletedUser);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
+export const updateUser = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const { id } = req.params;
+
+    const user = await getUserById(id);
+
+    // If the user with this id doesn't exist
+    if (!user) {
+      return res.sendStatus(400);
     }
-}
 
-export const deleteUser = async (req: express.Request, res: express.Response) => {
-    try {
-        const {id} = req.params;
-
-        const deletedUser = await deleteUserById(id);
-
-        return res.json(deletedUser);
-        
-    } catch (error) {
-        console.log(error);
-        return res.sendStatus(400);
+    for (const key in req.body) {
+      if (req.body[key]) {
+        user.set(key, req.body[key]);
+      }
     }
-}
+    await user.save();
 
-export const updateUser = async (req: express.Request, res: express.Response) => {
-    try {
-        const {username} = req.body;
-        const {id} = req.params;
-
-        if(!username) {
-            return res.sendStatus(400);
-        }
-
-        const user = await getUserById(id);
-        user.username = username;
-        await user.save();
-
-        return res.status(200).json(user).end();
-        
-    } catch (error) {
-        console.log(error);
-        return res.sendStatus(400);
-    }
-}
-
+    return res.status(200).json(user).end();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
--- src/db/users.ts
+++ src/db/users.ts
@@ -1,24 +1,49 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-    username: {type: String, required: true},
-    email: {type: String, required: true},
-    authentication: {
-        password: {type: String, required: true, select: false}, //Select fetches the whole auth object containing all users, we don't want that
-        salt: {type: String, select: false},
-        sessionToken: {type: String, select: false}
-    }
-})
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+  authentication: {
+    password: { type: String, required: true, select: false }, //Select fetches the whole auth object containing all users, we don't want that
+    salt: { type: String, select: false },
+    sessionToken: { type: String, select: false },
+  },
+  bio: String,
+  totalMovies: Number,
+  daysWatched: Number,
+  meanMovieScore: Number,
+  totalShows: Number,
+  episodesWatched: Number,
+  meanShowScore: Number,
+  favMovies: [String],
+  favShows: [String],
+  favCharacters: [String],
+  favStaff: [String],
+  dp: String,
+  bannerImg: String,
+  genreOverview: [
+    {
+      name: String,
+      numOfEntries: Number,
+    },
+  ],
+});
 
-export const UserModel = mongoose.model('User', UserSchema); 
+export type User = mongoose.InferSchemaType<typeof UserSchema>;
+
+export const UserModel = mongoose.model("User", UserSchema);
 
 export const getUsers = () => UserModel.find();
-export const getUserByEmail = (email: string) => UserModel.findOne({email});
-export const getUserBySessionToken = (sessionToken: string) => UserModel.findOne({
-    'authentication.sessionToken': sessionToken
-});
+export const getUserByEmail = (email: string) => UserModel.findOne({ email });
+export const getUserBySessionToken = (sessionToken: string) =>
+  UserModel.findOne({
+    "authentication.sessionToken": sessionToken,
+  });
+console.log(typeof UserModel);
 export const getUserById = (id: string) => UserModel.findById(id);
-export const createUser = (values: Record<string, any>) => new UserModel(values)
-    .save().then((user) => user.toObject());
-export const deleteUserById = (id: string) => UserModel.findOneAndDelete({_id: id})
-export const updateUserById = (id: string, values: Record<string, any>) => UserModel.findByIdAndUpdate(id, values);
+export const createUser = (values: Record<string, any>) =>
+  new UserModel(values).save().then((user) => user.toObject());
+export const deleteUserById = (id: string) =>
+  UserModel.findOneAndDelete({ _id: id });
+export const updateUserById = (id: string, values: Record<string, any>) =>
+  UserModel.findByIdAndUpdate(id, values);
