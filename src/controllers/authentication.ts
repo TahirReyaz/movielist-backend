38af41276499f117a3074cee70462592f4b7ab60
TahirReyaz <tahirreyaz5@gmail.com>
2023-12-31 15:23:51 +0530

    Sending better auth response

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -1,71 +1,82 @@
-import express from 'express';
+import express from "express";
 
-import { createUser, getUserByEmail } from '../db/users.js';
-import { authentication, random } from '../helpers/index.js';
+import { createUser, getUserByEmail } from "../db/users.js";
+import { authentication, random } from "../helpers/index.js";
 
 export const login = async (req: express.Request, res: express.Response) => {
-    try {
-        const {email, password} = req.body;
+  try {
+    const { email, password } = req.body;
 
-        if(!email || !password) {
-            return res.sendStatus(400);
-        }
-
-        const user = await getUserByEmail(email).select('+authentication.salt +authentication.password');
-        if(!user) {
-            console.log('User not found');
-            return res.sendStatus(400);
-        }
-
-        const expectedHash = authentication(user.authentication.salt, password);
-
-        if(user.authentication.password != expectedHash) {
-            console.log("Wrong password");
-            return res.sendStatus(403);
-        }
-
-        const salt = random();
-        // updating session token
-        user.authentication.sessionToken = authentication(salt, user._id.toString());
-
-        await user.save();
-
-        // set the cookie
-        res.cookie('TAHIR-AUTH', user.authentication.sessionToken, {domain: 'localhost', path: '/'})
-
-        return res.status(200).json(user).end();
-    } catch (error) {
-        console.log(error);
-        return res.sendStatus(400);
+    if (!email || !password) {
+      return res.sendStatus(400);
     }
-}
+
+    const user = await getUserByEmail(email).select(
+      "+authentication.salt +authentication.password"
+    );
+    if (!user) {
+      console.log("User not found");
+      return res.status(400).send({ message: "User not found" });
+    }
+
+    const expectedHash = authentication(user.authentication.salt, password);
+
+    if (user.authentication.password != expectedHash) {
+      console.log("Wrong password");
+      return res.status(403).send({ message: "Wrong Password" });
+    }
+
+    const salt = random();
+    // updating session token
+    user.authentication.sessionToken = authentication(
+      salt,
+      user._id.toString()
+    );
+
+    await user.save();
+
+    // set the cookie
+    res.cookie("MOVIELIST-AUTH", user.authentication.sessionToken, {
+      domain: "localhost",
+      path: "/",
+    });
+
+    return res
+      .status(200)
+      .json({ ...user, message: "Successfully logged in" })
+      .end();
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send({ message: "Error loggin in" });
+  }
+};
 
 export const register = async (req: express.Request, res: express.Response) => {
-    try {
-        const {email, password, username} = req.body;
+  try {
+    const { email, password, username } = req.body;
 
-        if(!email || !password || !username) {
-            return res.sendStatus(400);
-        }
-
-        const existingUser = await getUserByEmail(email);
-        if(existingUser) {
-            return res.sendStatus(400);
-        }
-
-        const salt = random();
-        const user = await createUser({
-            email, 
-            username,
-            authentication: {
-                salt, 
-                password: authentication(salt, password)
-            }
-        })
-
-        return res.status(200).json(user).end();
-    } catch (error) {
-        console.log(error);
-        return res.sendStatus(400);
+    if (!email || !password || !username) {
+      return res.sendStatus(400);
     }
-}
+
+    const existingUser = await getUserByEmail(email);
+    if (existingUser) {
+      return res.sendStatus(400);
+    }
+
+    const salt = random();
+    const user = await createUser({
+      email,
+      username,
+      authentication: {
+        salt,
+        password: authentication(salt, password),
+      },
+    });
+
+    return res.status(200).json(user).end();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
