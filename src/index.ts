f7c9e5a2a58dae76eb8b132f750944349ab3de81
TahirReyaz <tahirreyaz5@gmail.com>
2024-01-26 13:13:29 +0530

    removed http server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import http from "http";
 import bodyParser from "body-parser";
 import cors from "cors";
 import cookieParser from "cookie-parser";
@@ -19,8 +18,6 @@ app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
 
-const server = http.createServer(app);
-
 const port = process.env.PORT || 3000;
 
 mongoose.Promise = Promise;
@@ -30,6 +27,6 @@ mongoose.connection.on("error", (error: Error) => console.log(error));
 
 app.use("/", router());
 
-server.listen(port, () => {
+app.listen(port, () => {
   console.log("Server running on http://localhost:", port);
 });
