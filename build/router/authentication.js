a88fdcabd057f0534624289282cb4e6d55c61f22
TahirReyaz <tahirreyaz5@gmail.com>
2024-01-26 12:37:14 +0530

    Deleted build and changed start path

--- build/router/authentication.js
+++
@@ -1,5 +0,0 @@
-import { login, register } from '../controllers/authentication.js';
-export default (router) => {
-    router.post('/auth/register', register);
-    router.post('/auth/login', login);
-};
