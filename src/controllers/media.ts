3f2fdb1750e523f8ee10e59fbeff08c6b7a6dd00
TahirReyaz <tahirreyaz5@gmail.com>
2024-02-03 13:19:45 +0530

    Added search media feature

--- src/controllers/media.ts
+++ src/controllers/media.ts
@@ -2,6 +2,7 @@ import express from "express";
 import axios from "axios";
 
 const TMDB_ENDPOINT = "https://api.themoviedb.org/3";
+const TMDB_API_KEY = process.env.TMDB_API_KEY;
 
 export const getBulkMedia = async (
   req: express.Request,
@@ -10,7 +11,7 @@ export const getBulkMedia = async (
   try {
     const { mediaType, bulktype } = req.params;
     const response = await axios.get(
-      `${TMDB_ENDPOINT}/${mediaType}/${bulktype}?api_key=${process.env.TMDB_API_KEY}`
+      `${TMDB_ENDPOINT}/${mediaType}/${bulktype}?api_key=${TMDB_API_KEY}`
     );
     res.status(200).json(response.data.results);
   } catch (error) {
@@ -26,7 +27,7 @@ export const getMediaDetail = async (
   try {
     const { mediaType, mediaid } = req.params;
     const response = await axios.get(
-      `${TMDB_ENDPOINT}/${mediaType}/${mediaid}?api_key=${process.env.TMDB_API_KEY}`
+      `${TMDB_ENDPOINT}/${mediaType}/${mediaid}?api_key=${TMDB_API_KEY}`
     );
     try {
       res.status(200).json(response.data);
@@ -46,9 +47,12 @@ export const searchMulti = async (
 ) => {
   try {
     const { query } = req.params;
-    const response = await axios.get(
-      `${TMDB_ENDPOINT}/search/multi?query=${query}&api_key=${process.env.TMDB_API_KEY}`
-    );
+    const response = await axios.get(`${TMDB_ENDPOINT}/search/multi`, {
+      params: {
+        query,
+        api_key: TMDB_API_KEY,
+      },
+    });
     try {
       res.status(200).json(response.data);
     } catch (error) {
@@ -58,5 +62,46 @@ export const searchMulti = async (
   } catch (error) {
     console.error(error);
     return res.status(400).send({ message: error });
+  }
+};
+
+export const searchMedia = async (
+  req: express.Request<
+    { mediaType: string },
+    any,
+    any,
+    {
+      query?: string;
+      include_adult?: string;
+      language?: string;
+      page?: string;
+      year?: string;
+    }
+  >,
+  res: express.Response
+) => {
+  try {
+    const { mediaType } = req.params;
+    const { query, include_adult, language, page, year } = req.query;
+
+    if (mediaType !== "movie" && mediaType !== "tv") {
+      return res.status(400).json({ message: "Invalid media type" });
+    }
+
+    const response = await axios.get(`${TMDB_ENDPOINT}/search/${mediaType}`, {
+      params: {
+        query,
+        include_adult,
+        language,
+        page,
+        year,
+        api_key: TMDB_API_KEY,
+      },
+    });
+
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "SOMETHING WENT WRONG" });
   }
 };
--- src/router/media.ts
+++ src/router/media.ts
@@ -3,11 +3,13 @@ import express from "express";
 import {
   getBulkMedia,
   getMediaDetail,
+  searchMedia,
   searchMulti,
 } from "../controllers/media";
 
 export default (router: express.Router) => {
   router.get("/:mediaType/bulk/:bulktype", getBulkMedia);
   router.get("/:mediaType/detail/:mediaid", getMediaDetail);
-  router.get("/search/:query", searchMulti);
+  router.get("/search/multi/:query", searchMulti);
+  router.get("/search/:mediaType", searchMedia);
 };
