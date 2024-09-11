fde850ee73551e35fec17f68736bc0415922cf4b
TahirReyaz <tahirreyaz5@gmail.com>
2024-09-11 16:49:05 +0530

    Showing correct credits on staff page

--- src/controllers/staff.ts
+++ src/controllers/staff.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import axios from "axios";
 import { TMDB_API_KEY, TMDB_ENDPOINT } from "../constants/misc";
-import { divideMediaByMediaType, divideMediaByYear } from "../helpers";
+import { divideMediaByYear } from "../helpers";
+import { isCallOrNewExpression } from "typescript";
 
 export const getStaffDetail = async (
   req: express.Request,
@@ -37,10 +38,30 @@ export const getStaffCredits = async (
       }
     );
 
-    const credits = response.data;
-    const dividedMedia = divideMediaByYear(credits.cast);
+    const cast = response.data?.cast;
+    const crew = response.data?.crew;
+    let credits: any[] = [];
+    if (cast) {
+      credits = [...cast];
+    }
+    if (crew) {
+      const crewMap = new Map();
+      crew.forEach((media: any) => {
+        const mediaid = media.id;
+        if (crewMap.has(mediaid)) {
+          const oldJob = crewMap.get(mediaid).job;
+          const newJob = `${oldJob} & ${media.job}`;
+          crewMap.set(mediaid, { ...media, job: newJob });
+        } else {
+          crewMap.set(mediaid, media);
+        }
+      });
+      const uniqueCrewMedia = Array.from(crewMap, ([mediaid, media]) => media);
+      credits = [...credits, ...uniqueCrewMedia];
+    }
+    const dividedCredits = divideMediaByYear(credits);
 
-    res.status(200).json({ credits: dividedMedia });
+    res.status(200).json({ credits: dividedCredits });
   } catch (error) {
     console.error(error);
     return res.status(400).send({ message: error });
